b01f835990219ead3474acc49f11e3aac4e0a245
this is yunchi <apple@appledeMacBook-Pro-2.local>
2014-08-30 15:54:00 +0800

    Rails_girl_course
    0830

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :ideas
+  devise_for :users
+  resources :ideas,:articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :articles
+
 
   # You can have the root of your site routed with "root"
    root 'ideas#index'
